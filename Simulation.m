8634cdcccbb17b6498d3c96bcfd9b630d009ad34
jthntr04 <97707447+jthntr04@users.noreply.github.com>
2022-04-21 15:24:05 -0600

    askld;jf

--- Simulation.m
+++ Simulation.m
@@ -1,3 +1,6 @@
+%% Set Up Command Window
+clc; clear; close all;
+
 %% Constants
 Kg = 33.3; %Gear Ratio
 Km = 0.0401; %Motor Constant [NM\amp]
@@ -16,7 +19,7 @@ fc = 1.8; %Natural Frequency [Hz]
 Kstiff = (2*pi*fc)^2*(Jl);
 
 %% Maximum Overshoot 
-Mp = 0.2; %20 percent 
+Mp = 1.2*0.12; %20 percent 
 zeta = sqrt((log(Mp))^2/(pi^2+(log(Mp))^2));
 %% Settling Time
 % 5%
@@ -40,7 +43,10 @@ sysTF = tf(num,den);
 [x,t] = step(sysTF);
 %% Import Data 
 testData = load('testData');
+A = (testData(2494:4500,1)-11925000)/1000;
+B = testData(2494:4500,2)+(-1*testData(2493,2));
 testData2 = load('testData2');
+
 % A = load('April72022/rigid_5_kp_5_kd_0'); %Kp = 5, Kd = 0
 % B = load('April72022/rigid_5_kp_10_kd_0'); %Kp = 10, Kd = 0
 % C = load('April72022/rigid_5_kp_15_kd_1pt5'); %Kp = 15, Kd = 1
@@ -52,38 +58,50 @@ testData2 = load('testData2');
 
 %% Plotting
 figure()
-hold on
-plot(t,x);
-xline(ts);
-yline(Mp);
-ylim([0,0.3]);
-title('Model Response');
+hold on 
+title('Model Vs Experimental');
+plot(t,x*4.9);
+plot(A,B);
 xlabel('Time');
-ylabel('Position');
-legend('Response', 'Settling Time', 'Maximum Overshoot');
+ylabel('Angular Position');
+legend('Model', 'Experimental');
 hold off
 
-figure()
-hold on
-plot((testData(2494:10000,1)-11925000)/1000, testData(2494:10000,2));
-xline(1);
-yline(1.2*0.3);
-title('Experimental Response');
-xlabel('Time');
-ylabel('Position');
-legend('Response', 'Settling Time', 'Maximum Overshoot');
-hold off
-
-figure()
-hold on
-plot((testData2(:,1)-13402752)/1000, testData2(:,2));
-xline(1);
-yline(1.2*0.3);
-title('Experimental Response');
-xlabel('Time');
-ylabel('Position');
-legend('Response', 'Settling Time', 'Maximum Overshoot');
-hold off
+% figure()
+% hold on
+% plot(t,x*3);
+% plot((testData(2494:10000,1)-11925000)/1000, testData(2494:10000,2));
+% xline(ts);
+% yline(Mp);
+% ylim([0,0.3]);
+% title('Model Response');
+% xlabel('Time');
+% ylabel('Position');
+% legend('Response', 'Settling Time', 'Maximum Overshoot');
+% hold off
+% 
+% figure()
+% hold on
+% %plot((testData(2494:10000,1)-11925000)/1000, testData(2494:10000,2)+(-1*testData(2494,2))); %+0.319
+% plot(A,B);
+% xline(1);
+% yline(1.2*0.3);
+% title('Experimental Response');
+% xlabel('Time');
+% ylabel('Position');
+% legend('Response', 'Settling Time', 'Maximum Overshoot');
+% hold off
+% 
+% figure()
+% hold on
+% plot((testData2(2249:10000,1)-13405000)/1000, testData2(2249:10000,2));
+% xline(1);
+% yline(1.2*0.3);
+% title('Modified Response');
+% xlabel('Time');
+% ylabel('Position');
+% legend('Response', 'Settling Time', 'Maximum Overshoot');
+% hold off
 
 
 
